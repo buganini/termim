619bb5558d05a2ce083367d8ce0947f82f8f59cf
Buganini Q <buganini@gmail.com>
2011-05-21 23:47:55 +0800

    preserve old approach in comment

--- term.c
+++ term.c
@@ -98,6 +98,7 @@ ssize_t term_write(struct term *term, const char *ibuf, size_t len){
 				write(term->out, ibuf+j, i-j+1);
 				j=i+1;
 				//term_get_cursor(term);
+				//term->cur_col+=term->off_col;
 				term->cur_row+=1;
 				term->cur_col=1+term->off_col;
 				term_put_cursor(term);
