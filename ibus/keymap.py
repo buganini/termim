ccc5ec10bb4559ea3692c66c3489863ac436f7e8
Buganini Q <buganini@gmail.com>
2011-05-22 20:57:17 +0800

    keymap.py: return modifiers information

--- ibus/keymap.py
+++ ibus/keymap.py
@@ -16,6 +16,7 @@
 
 import os
 import re
+from ibus.modifier import *
 
 class keymap():
 	def __init__(self, keymap):
@@ -28,22 +29,22 @@ class keymap():
 				continue
 			(kcode, kb, ks, kc, kcs, ka, kas, kac, kacs, kl) = re.findall(r"('.'|\w+)", l)
 			kcode=int(kcode)
-			if(kb[0]=="'" and kb[2]=="'"):
-				self.keysym_to_keycode_map[kb[1]]=(kcode,0)
-			if(ks[0]=="'" and ks[2]=="'"):
-				self.keysym_to_keycode_map[ks[1]]=(kcode,0)
-			if(kc[0]=="'" and kc[2]=="'"):
-				self.keysym_to_keycode_map[kc[1]]=(kcode,0)
-			if(kcs[0]=="'" and kcs[2]=="'"):
-				self.keysym_to_keycode_map[kcs[1]]=(kcode,0)
-			if(ka[0]=="'" and ka[2]=="'"):
-				self.keysym_to_keycode_map[ka[1]]=(kcode,0)
-			if(kas[0]=="'" and kas[2]=="'"):
-				self.keysym_to_keycode_map[kas[1]]=(kcode,0)
-			if(kac[0]=="'" and kac[2]=="'"):
-				self.keysym_to_keycode_map[kac[1]]=(kcode,0)
 			if(kacs[0]=="'" and kacs[2]=="'"):
-				self.keysym_to_keycode_map[kacs[1]]=(kcode,0)
+				self.keysym_to_keycode_map[kacs[1]]=(kcode, ALT_MASK|CONTROL_MASK|SHIFT_MASK)
+			if(kac[0]=="'" and kac[2]=="'"):
+				self.keysym_to_keycode_map[kac[1]]=(kcode, ALT_MASK|CONTROL_MASK)
+			if(kas[0]=="'" and kas[2]=="'"):
+				self.keysym_to_keycode_map[kas[1]]=(kcode, ALT_MASK|SHIFT_MASK)
+			if(ka[0]=="'" and ka[2]=="'"):
+				self.keysym_to_keycode_map[ka[1]]=(kcode, ALT_MASK)
+			if(kcs[0]=="'" and kcs[2]=="'"):
+				self.keysym_to_keycode_map[kcs[1]]=(kcode, CONTROL_MASK|SHIFT_MASK)
+			if(kc[0]=="'" and kc[2]=="'"):
+				self.keysym_to_keycode_map[kc[1]]=(kcode, CONTROL_MASK)
+			if(ks[0]=="'" and ks[2]=="'"):
+				self.keysym_to_keycode_map[ks[1]]=(kcode, SHIFT_MASK)
+			if(kb[0]=="'" and kb[2]=="'"):
+				self.keysym_to_keycode_map[kb[1]]=(kcode, 0)
 		f.close()
 
 	def keysym_to_keycode(self, sym):
