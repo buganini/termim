5fc01e456cd0ffcceff9855e04627b49434363cf
Buganini Q <buganini@gmail.com>
2011-05-23 05:36:03 +0800

    display editing buffer

--- termim-chewing.c
+++ termim-chewing.c
@@ -1,8 +1,10 @@
 #include <errno.h>
 #include <limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
 #include <unistd.h>
 #include <sys/select.h>
 #include <sys/types.h>
@@ -10,13 +12,36 @@
 #include <chewing/chewing.h>
 #include "keymap.h"
 
+ChewingContext *ctx;
 int out;
 int active=0;
+char *chewing_buf;
 
 static int selKey_define[ 11 ] = {'1','2','3','4','5','6','7','8','9','0',0};
 
+struct winsize win;
+
+void
+winch(int sig){
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &win);
+}
+
+void draw(){
+	char tbuf[64];
+	printf("\033[H");
+	if(active){
+		printf("[注音] ");
+	}else{
+		printf("[英數] ");
+	}
+	chewing_buf=chewing_buffer_String(ctx);
+	sprintf(tbuf,"%%-%ds", win.ws_col-7);
+	printf(tbuf, chewing_buf);
+	printf("\n");
+	fflush(stdout);
+}
+
 int main(int argc, char *argv[]){
-	ChewingContext *ctx;
 	char *s;
 	unsigned char ibuf[BUFSIZ];
 	char buf[128];
@@ -29,6 +54,9 @@ int main(int argc, char *argv[]){
 
 	out=strtol(argv[1], NULL, 10);
 
+	signal(SIGWINCH, &winch);
+	winch(0);
+
 	chewing_Init( "/usr/local/share/chewing", "/tmp" );
 	ctx = chewing_new();
 	chewing_set_KBType( ctx, chewing_KBStr2Num( "KB_DEFAULT" ) );
@@ -37,7 +65,9 @@ int main(int argc, char *argv[]){
 	chewing_set_addPhraseDirection( ctx, 1 );
 	chewing_set_selKey( ctx, selKey_define, 10 );
 	chewing_set_spaceAsSelection( ctx, 1 );
-	
+
+	printf("\033[H\033[44m\n\n");
+	draw();
 
 	FD_ZERO(&rfd);
 	while(1){
@@ -117,6 +147,7 @@ int main(int argc, char *argv[]){
 				write(out, ibuf, n);
 			}
 		}
+		draw();
 	}
 
 	chewing_delete(ctx);
